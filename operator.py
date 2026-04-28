b19738e1e43d1b4d9af2bfd4e6b9c9b63a9f26a6
shaignizamzada <shaiqnizamzade@gmail.com>
2026-04-28 16:15:25 +0400

    operator 2 - logical operators and input ( lesson 5),

--- operator.py
+++ operator.py
@@ -1,25 +1,6 @@
-#  Arithmetic Operators
-
-
-a = 10 
-b = 3
-
-# print (a + b) 
-# print (a - b) 
-# print (a * b) 
-# print (a / b) 
-# print (a//b)
-# print (a % b)
-# print (a ** b)
-
-#  Comparison Operators
-
-x = 5 
-y = 10
-print (x == y)
-print (x != y)
-print (x > y)
-print (x < y)
-print (x >= 5)
-print (x <= 5)
-
+False
+True
+False
+True
+True
+True
---
+++ operator2.py
@@ -0,0 +1,15 @@
+a = 5
+b = 7
+
+# Logical AND operator
+print (a > 0 and b > 0)
+
+# Logical OR operator
+print (a > 0 or b < 0)
+
+# Logical NOT operator
+print (not (a > 0))
+
+yas = int(input("Yaşınızı giriniz: "))
+ad = str(input("Adınızı giriniz: "))
+print ("Benim adim", ad , "ve", yas, "yaşındayım.")
