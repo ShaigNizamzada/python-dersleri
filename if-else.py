414d4d957941872fc23bbf0de690938e022121c3
shaignizamzada <shaiqnizamzade@gmail.com>
2026-04-28 16:43:20 +0400

    if-else (lesson 6)

---
+++ if-else.py
@@ -0,0 +1,28 @@
+# sayi = 10
+
+# if sayi > 0:    
+#     print("Sayı pozitiftir.")
+# elif sayi < 0:
+#     print("Sayı negatiftir.")
+# else:
+#     print("Sayı sıfırdır.")
+
+
+# not_ort = int(input("Not ortalamanızı giriniz: "))
+
+# if not_ort >= 90:
+#     print("Notunuz: AA")
+# elif not_ort >= 80:
+#     print("Notunuz: BA")
+# elif not_ort >= 70:
+#     print("Notunuz: BB")
+# elif not_ort >= 60:
+#     print("Notunuz: CB")
+# elif not_ort >= 50:
+#     print("Notunuz: CC")
+# elif not_ort >= 40:
+#     print("Notunuz: DC")
+# elif not_ort >= 30:
+#     print("Notunuz: DD")
+# else:
+#     print("Notunuz: FF")
