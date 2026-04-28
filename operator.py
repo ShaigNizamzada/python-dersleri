5b3cfaa72d42a7c9fee08fe2d9668fdb596a7196
shaignizamzada <shaiqnizamzade@gmail.com>
2026-04-28 16:03:21 +0400

    veritipleri2 (lesson3), operator (lesson4) lessons

---
+++ operator.py
@@ -0,0 +1,25 @@
+#  Arithmetic Operators
+
+
+a = 10 
+b = 3
+
+# print (a + b) 
+# print (a - b) 
+# print (a * b) 
+# print (a / b) 
+# print (a//b)
+# print (a % b)
+# print (a ** b)
+
+#  Comparison Operators
+
+x = 5 
+y = 10
+print (x == y)
+print (x != y)
+print (x > y)
+print (x < y)
+print (x >= 5)
+print (x <= 5)
+
---
+++ veri-tipleri2.py
@@ -0,0 +1,18 @@
+sayi = 10
+pi = 3.14
+isim = "Shaig"
+aktiv =  True
+
+
+deaktiv = False
+
+print(type(sayi))
+print(type(pi))
+print(type(isim))
+print(type(aktiv))
+print(type(deaktiv))
+
+ad = "Aram"
+yas = 25
+
+print("Benim adim", ad, "ve", yas, "yasindayim.")
